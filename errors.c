25eb16e8a562e53b6afd9e6aa7c53bf75c18c37c
SiSimo9750 <133168307+SiSimo9750@users.noreply.github.com>
2023-08-23 23:12:23 +0100

    Create errors.c

---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ *_eputs - display a string
+ * @str: the input string
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int j = 0;
+
+	if (!str)
+		return;
+	while (str[j] != '\0')
+	{
+		_eputchar(str[j]);
+		j++;
+	}
+}
+
+/**
+ * _eputchar - writes c to stderr
+ * @c: input char
+ *
+ * Return: 1 on success.
+ * -1 On error, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int j;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, j);
+		j = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[j++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes c to given fd
+ * @c: The input character
+ * @fd: The filedescriptor to destination
+ *
+ * Return: 1 on success.
+ * -1 on error, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int j;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, j);
+		j = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[j++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - display an input string
+ * @str: the string input
+ * @fd: the filedescriptor
+ *
+ * Return: chars put number
+ */
+int _putsfd(char *str, int fd)
+{
+	int j = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		j += _putfd(*str++, fd);
+	}
+	return (j);
+}
