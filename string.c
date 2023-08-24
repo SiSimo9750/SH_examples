c7fb98a8aa18f5f0d7e6770e0911a1f47a4d35f6
SiSimo9750 <133168307+SiSimo9750@users.noreply.github.com>
2023-08-24 02:45:34 +0100

    Create string.c

---
+++ string.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - calclate the string length
+ * @s: the input string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		j++;
+	return (j);
+}
+
+/**
+ * _strcmp - performs comparison of two strings.
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with -erifyif needle starts with haystack
+ * @haystack: to search
+ * @needle: to look for
+ *
+ * Return: address of next char of haystack
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *r = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (r);
+}
